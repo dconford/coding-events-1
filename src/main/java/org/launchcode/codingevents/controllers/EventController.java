2de7c28a59a3b1602f90b23348d07b229bf33314
Daniel C <daniel@daniel.com>
2019-12-20 11:39:46 -0600

    working through practice videos

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -17,13 +19,39 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
+        Map<String, String> eventMap = new HashMap<>();
+        eventMap.put("Code With Pride", "A 2 day seminar focused on supporting alternative people and pathways to coding.");
+        eventMap.put("Strange Loop", "Learn about the interaction of small food businesses and the programming world");
+        eventMap.put("Apple WWDC", "The Premier conference for Apple Developers. Focused seminars and " +
+                "presentations from the best in the Apple universe.");
+        //context.setVariable("eventMap", eventMap);
+//        List<String> events = new ArrayList<>();
+//        events.add("Code With Pride");
+//        events.add("Strange Loop");
+//        events.add("Apple WWDC");
+//        events.add("SpringOne Platform");
+//        model.addAttribute("events", events);
+       // addAllAttributes(Map<String,?> attributes)
+       //model.addAllAttributes("eventNames", "eventDescriptions");
+    //put("eventNames", eventMap.keySet());
+       model.addAttribute("eventDescriptions", eventMap.values());
+       model.addAttribute("events", eventMap.entrySet());
         return "events/index";
     }
 
 }
+
+
+
+//    @GetMapping
+//    public String displayAllEvents (Model model) {
+////        List<String> events = new ArrayList<>();
+////        events.add("Code With Pride");
+////        events.add("StrangeLoop");
+////        events.add("Apple WWDC");
+////        events.add("SpringOne Platform");
+////        events.add("Geek Gala 2020");
+////        events.add("MoonShot Awards 2020");
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
